3e46908435737f89e6386a51c79d007acc734697
pkumarbe <mymail8500@gmail.com>
2020-10-11 06:27:23 -0400

    updated app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Welcome to Devops jenkins sesson"
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=int('8001'), debug=True,threaded=True, use_reloader=True)
