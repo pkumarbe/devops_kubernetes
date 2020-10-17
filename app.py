1ef600bbaeefe49912f4789d3db0dfd53d9cab46
pkumarbe <mymail8500@gmail.com>
2020-10-17 08:21:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome to Devops jenkins 2nd page "
+    return "Welcome to Devops jenkins 3rd page "
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=int('8001'), debug=True,threaded=True, use_reloader=True)
